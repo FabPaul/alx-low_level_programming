6945073847c3865734502efbc2be986ef8ec28dd
FabPaul <paulfopa0@gmail.com>
2023-02-16 14:54:19 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints with a std error
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+		return (1);
+}
