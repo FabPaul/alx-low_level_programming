d6777edc6d1c04fd8c3c96b3156579a398052a07
FabPaul <paulfopa0@gmail.com>
2023-02-16 15:14:33 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints with a std error
+ * main - A program that prints with new line and EOF
  * Return: 0 (Success)
  */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-		return (1);
+	return (1)
 }
