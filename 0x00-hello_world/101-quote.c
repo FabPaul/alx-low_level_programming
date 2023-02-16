f4ca4d930d25f2790d23878cb2dd4caf06cd2aa1
FabPaul <paulfopa0@gmail.com>
2023-02-16 15:20:14 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
- * main - A program that prints with new line and EOF
+ * main - Program that prints with new line &  EOF
  * Return: 0 (Success)
  */
 int main(void)
 {
-	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1)
+	scanf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
 }
