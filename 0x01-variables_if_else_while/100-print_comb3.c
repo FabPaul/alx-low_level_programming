09074e83cb1d905b1aa54e08762c37c28f15db1c
FabPaul <paulfopa0@gmail.com>
2023-02-17 13:16:14 -0800

    0x01. C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - different combinations of numbers
+ * Result: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = a+1; b <10; b++)
+		{
+	if (b != d)
+	{
+		putchar(a);
+		putchar(b);
+
+		if (a == 8 && b == 9)
+			continue;
+		putchar(',');
+		putchar(' ')
+	}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
