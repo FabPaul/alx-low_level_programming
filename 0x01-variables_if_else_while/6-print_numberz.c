15e4e1e2d579c53f19ecc3010140553973131e95
FabPaul <paulfopa0@gmail.com>
2023-02-17 12:05:29 -0800

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,17 +2,17 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Prints base 10 using char
+ * main - Prints base 10 single digits
  * Return: 0 (Success)
  */
 int main(void)
 {
 	int b10;
 
-		for (b10 = 0; b10 < 10; b10++)
+		for (b10 = '0'; b10 < '10'; b10++)
 
-			putchar((b10 % 10) + "0");
+			putchar(b10)
 
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
