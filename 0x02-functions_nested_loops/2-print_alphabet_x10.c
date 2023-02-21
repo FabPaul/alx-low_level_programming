fe8b16f3f59ea9560a05c87a9d0c5ce3f2811e97
FabPaul <paulfopa0@gmail.com>
2023-02-21 09:04:05 -0800

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alpha in hexa
+ * 
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	char d;
+	int i = 0;
+
+	while (i <= 9)
+	{
+		for (d = 'a'; d <= 'z'; d++)
+		{
+			_putchar(d);
+		}
+		_putchar('\n');
+		i++;
+	}
+}
