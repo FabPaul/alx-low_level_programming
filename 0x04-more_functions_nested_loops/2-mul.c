7979c4ef944b331c0c1df417d5fb70e1bf7dc88b
FabPaul <paulfopa0@gmail.com>
2023-02-23 03:16:29 -0800

    Multiplying 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - To multiply 2 characters
+ * @a: first character
+ * @b: second character
+ * Return: Results after multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
