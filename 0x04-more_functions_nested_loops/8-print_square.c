7e22eaba67b93a194fcd336960ccb7084b17bc74
FabPaul <paulfopa0@gmail.com>
2023-02-23 06:56:30 -0800

    Printing a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - Print a square then a new line
+ * @size: The size of quare
+ * Return: The square
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar('\n');
+		}
+	}
+}
