fa4adc9927c0c28019cc85c37e2dd98c0ac03a9d
FabPaul <paulfopa0@gmail.com>
2023-02-23 05:18:06 -0800

    Print new line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - Function that draws a straight line
+ * @n: The number of times to print the line
+ * Return: new line
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar(95);
+		}
+		_putchar('\n');
+	}
+}
