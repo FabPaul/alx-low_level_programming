ed62425195ed1a6d987d37ba669b40bc82dfe60b
FabPaul <paulfopa0@gmail.com>
2023-03-20 12:00:33 -0700

    Dog's element

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Program that defines a dog's elements
+ * @name: Dog's name
+ * @age: Dog's age
+ * @owner: Dog's owner
+ * Return: Dog's elements
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
