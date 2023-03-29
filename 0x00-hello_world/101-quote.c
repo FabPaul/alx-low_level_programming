5ecbc49ce6b0bdb3c652d2739e81390091047267
FabPaul <paulfopa0@gmail.com>
2023-03-29 04:08:03 -0700

    Function that prints an output using th write() function

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <string.h>
+#include <stdio.h>
 
 /**
  * main - Function that prints an output using the write() function
@@ -8,7 +9,8 @@
 
 int main(void)
 {
-	char* output = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(STDOUT_FILENO, output, strlen(output));
+	char *print = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(STDOUT_FILENO, print, strlen(print));
 	return (1);
 }
