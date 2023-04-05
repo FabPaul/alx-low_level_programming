831799acc9118544a2423b79ed66712ab885f88e
FabPaul <paulfopa0@gmail.com>
2023-04-05 07:39:42 -0700

    Function that prints all combs of single digits

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
- * main - Combination of single digits
+ * main - Function that prints all combination of single digits
  * Return: 0 (Success)
  */
 int main(void)
 {
 	int c;
 
-	for (c = 48; c < 58; c++)
+	for (c = 0; c < 10; c++)
 	{
-		putchar(c);
-		if (c < 57)
+		putchar('0' + c);
+		if (c < 9)
 		{
-			putchar(44);
-			putchar(32);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 
