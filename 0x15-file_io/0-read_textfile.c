b7999524c65bf50de3ddb34a5a2af136cd5798ce
FabPaul <paulfopa0@gmail.com>
2023-04-10 05:46:53 -0700

    Function that reads a text file and prints it to the POSIX standard output

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * read_textfile - Function that reads a text file, prints to POSIX stdout
+ * @filename: file to be read
+ * @letters: Number of letters to read and print
+ * Return: read file
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char *buffer;
+	ssize_t reads;
+
+	if (!filename)
+	{
+		return(0);
+	}
+
+
+	fp = fopen(filename, "r");
+
+	if (fp == NULL)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+
+	if (!buffer)
+	{
+		fclose(fp);
+		return (0);
+	}
+
+	reads = fread(buffer, sizeof(char), letters, fp);
+
+	if (reads == -1)
+	{
+		fclose(fp);
+		free(buffer);
+		return (0);
+	}
+
+	fwrite(buffer, sizeof(char), reads, stdout);
+
+	fclose(fp);
+	free(buffer);
+
+	return (reads);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,11 @@
+0x15- C file i/o
+
+in this project, there are lots of tasks. they include writing a function that;
+
+1, Reads a text file and prints it to the POSIX standard output
+
+2, Creates a file
+
+3, appends text at the end of a file
+
+4, copies the content of a file to another
